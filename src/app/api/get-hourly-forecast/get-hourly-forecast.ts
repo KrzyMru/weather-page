9b2e934291091aa16b2b1cddc781a6275ceed30e
KrzyMru <krzymru20@gmail.com>
2025-11-28 16:01:24 +0100

    Fixed the getHourlyForecast API call

--- src/app/api/get-hourly-forecast/get-hourly-forecast.ts
+++ src/app/api/get-hourly-forecast/get-hourly-forecast.ts
@@ -3,7 +3,7 @@ import type { HourlyForecastResponse, GetHourlyForecastProps } from "./types";
 const getHourlyForecast = async (props: GetHourlyForecastProps): Promise<HourlyForecastResponse> => {
     const { latitude, longitude } = { ...props }
     
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?models=best_match&latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weather_code,wind_speed_10m,apparent_temperature,precipitation,relative_humidity_2m`);
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?models=best_match&latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weather_code`);
     const data = await response.json() as HourlyForecastResponse;
 
     return data;
