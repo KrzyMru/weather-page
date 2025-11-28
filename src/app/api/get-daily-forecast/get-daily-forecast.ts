46b9f4913e397452d307744f5350d3014ab32f5b
KrzyMru <krzymru20@gmail.com>
2025-11-28 15:41:29 +0100

    Fixed the getDailyForecast API call

--- src/app/api/get-daily-forecast/get-daily-forecast.ts
+++ src/app/api/get-daily-forecast/get-daily-forecast.ts
@@ -3,7 +3,7 @@ import type { DailyForecastResponse, GetDailyForecastProps } from "./types";
 const getDailyForecast = async (props: GetDailyForecastProps): Promise<DailyForecastResponse> => {
     const { latitude, longitude } = { ...props }
     
-    const response = await fetch(`https://api.open-meteo.com/v1/forecast?models=best_match&latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weather_code,wind_speed_10m,apparent_temperature,precipitation,relative_humidity_2m`);
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?models=best_match&latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min`);
     const data = await response.json() as DailyForecastResponse;
 
     return data;
